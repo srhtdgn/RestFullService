316f0940ea1f7247f2d72dfd6ba7fd95cd9f7cdd
Serhat DOĞAN <doganspr@hotmail.com>
2020-03-05 11:12:46 +0300

    Sunucu oluşturuldu ve Yazdir Metodu Eklendi

---
+++ RestFullService/ISunucu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace RestFullService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ISunucu" in both code and config file together.
+    [ServiceContract]
+    public interface ISunucu
+    {
+        [OperationContract]
+        void DoWork();
+
+        [OperationContract]
+        string Yazdir();
+    }
+}
