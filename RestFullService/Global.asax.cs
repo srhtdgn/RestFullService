a05b9aaa549571d82953ec12084572d98a497b3b
Serhat DOĞAN <doganspr@hotmail.com>
2020-03-05 11:50:19 +0300

    Global asax taki istek ayarları yapıldı.

--- RestFullService/Global.asax.cs
+++ RestFullService/Global.asax.cs
@@ -22,6 +22,13 @@ namespace RestFullService
 
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
+            if (HttpContext.Current.Request.HttpMethod== "OPTIONS")
+            {
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type");
+                HttpContext.Current.Response.End();
+            }
 
         }
 
